9648f6b43e46f58fa44644222c65c0dc8b9e0365
deepam-kapur <deepam@queryit.io>
2024-03-06 17:16:15 +0530

    [ADD] Refund contract changed

--- scripts/refundSplitBill.ts
+++ scripts/refundSplitBill.ts
@@ -5,9 +5,9 @@ import { NetworkProvider, sleep } from '@ton/blueprint';
 export async function run(provider: NetworkProvider, args: string[]) {
     const ui = provider.ui();
 
-    // TON Scan - https://tonscan.org/address/EQC9bflrgA3tOn24f0FTwDicysBiRMiHCxcYFrVhSONVO3hv
+    // TON Scan - https://tonscan.org/address/EQAlXGyEPdsCZON7BjVqizB0k6Kvs84o5ttDJ_jpnojeQ_Cm
 
-    const splitBillAdress= Address.parse('EQC9bflrgA3tOn24f0FTwDicysBiRMiHCxcYFrVhSONVO3hv');
+    const splitBillAdress= Address.parse('EQAlXGyEPdsCZON7BjVqizB0k6Kvs84o5ttDJ_jpnojeQ_Cm');
     console.log('Connecting to Contract - ', splitBillAdress)
 
     if (!(await provider.isContractDeployed(splitBillAdress))) {
@@ -17,13 +17,19 @@ export async function run(provider: NetworkProvider, args: string[]) {
 
     const splitBill = provider.open(SplitBill.fromAddress(splitBillAdress));
 
-    const amount = await ui.input('Amount to send?');
+    // await splitBill.send(
+    //     provider.sender(),
+    //     {
+    //         value: toNano("0.1"),
+    //     }, "Gas"
+    // );
 
+    const amount = await ui.input('Amount to send?');
     await splitBill.send(
         provider.sender(),
         {
             value: toNano(amount),
-        },"Refund 99.9%"
+        }, "Refund 99.9%"
     );
 
     ui.clearActionPrompt();
